eaf36e15a45cae11b88e2462350d169c9e4a2c0a
Pupiserbot <117370969+Pupiserbot@users.noreply.github.com>
2024-12-23 19:42:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: 'YOUR SESSION ID',
+SESSION_ID: 'HANSAMAL-MD=GQEWGShC#wUyR25KNg0URVRN-xkO-2EjX1O9--wotAMDc6qKtxwQ',
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'YOUR GIT TOKEN' : process.env.GITHUB_AUTH_TOKEN,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'YOUR GIT USER NAME' : process.env.GITHUB_USER_NAME,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? 'YOUR NUMBER' : process.env.OWNER_NUMBER
